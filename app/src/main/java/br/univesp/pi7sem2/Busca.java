2e9f4f0607e4638341724d450ac82ee3dd1bbd54
Milton Atarassi <milton.atarassi@aluno.univesp.br>
2017-11-11 12:28:14 -0200

    update 11/11/2017

--- app/src/main/java/br/univesp/pi7sem2/Busca.java
+++ app/src/main/java/br/univesp/pi7sem2/Busca.java
@@ -5,19 +5,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.Toast;
 
 
 public class Busca extends Fragment {
     SearchView searchView;
+    View myView;
+    Button myButton;
+    boolean isUp;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.busca, null);
 
         searchView=(SearchView) view.findViewById(R.id.searchView);
         searchView.setQueryHint("Busca");
         searchView.setIconifiedByDefault(true);
+
+        myView = view.findViewById(R.id.my_view);
+        myButton = (Button) view.findViewById(R.id.my_button);
+
+        myView.setVisibility(View.GONE);
+        isUp = false;
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isUp) {
+
+                    myView.setVisibility(View.GONE);
+
+                } else {
+                    myView.setVisibility(View.VISIBLE);
+
+                }
+                isUp = !isUp;
+
+            }
+        });
+
+
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
@@ -38,4 +68,5 @@ public class Busca extends Fragment {
 
     return view;
     }
+
 }
