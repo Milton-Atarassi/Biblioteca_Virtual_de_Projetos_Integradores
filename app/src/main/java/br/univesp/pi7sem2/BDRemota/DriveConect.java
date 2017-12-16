580c253f63b8b85be29fd5cda037a65011207898
Milton Atarassi <milton.atarassi@aluno.univesp.br>
2017-12-16 13:18:03 -0200

    update 12/12/2017

--- app/src/main/java/br/univesp/pi7sem2/BDRemota/DriveConect.java
+++ app/src/main/java/br/univesp/pi7sem2/BDRemota/DriveConect.java
@@ -1,30 +1,31 @@
 package br.univesp.pi7sem2.BDRemota;
-import android.app.*;
+
+import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
-import android.os.*;
-import android.support.v7.app.ActionBarActivity;
-import android.widget.*;
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import org.apache.http.*;
-import org.apache.http.client.*;
-import org.apache.http.client.methods.*;
-import org.apache.http.impl.client.*;
-import org.apache.http.protocol.*;
-import android.view.View.*;
-import android.view.*;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.widget.SearchView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.univesp.pi7sem2.BancoDeDados.TestAdapter;
 
 public class DriveConect extends IntentService
 {
-    int status;
-    //Context context;
-
     private static final String PATH_TO_SERVER =
-    "https://docs.google.com/spreadsheets/d/1Hpur-ezEXlFzjM-XleCyi02K9UuOuBa5ZeiOG_BcQWM/export?format=csv";
+            "https://docs.google.com/spreadsheets/d/ID DO ARQUIVO/export?format=csv";
+    //Context context;
+    int status;
     SearchView searchView;
 
     /**
@@ -56,32 +57,17 @@ public class DriveConect extends IntentService
         printCVSContent(file);
     }
 
-
-    private class DownloadFilesTask extends AsyncTask<URL, Void, List<String>> {
-        protected List<String> doInBackground(URL... urls) {
-            return downloadRemoteTextFileContent();
-        }
-        protected void onPostExecute(List<String> result) {
-            if(result != null){
-                printCVSContent(result);
-            //fileContent.setText("download feito com sucesso");
-                if(status!=HttpURLConnection.HTTP_OK){
-                  //  Toast.makeText(getApplication(),"no connection",Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-    private void printCVSContent(List<String> result){
-        try{
+    private void printCVSContent(List<String> result) {
+        try {
             String csvLine = "";
 
 
-                for(String row:result) {
-                    csvLine += row + " ";
-                    csvLine += "\n";
-                }
+            for (String row : result) {
+                csvLine += row + " ";
+                csvLine += "\n";
+            }
 
-                        try {
+            try {
                 File externalStorageDir = Environment.getExternalStorageDirectory();
                 File file = new File(externalStorageDir + "/bd_csv.csv");
                 FileWriter f = new FileWriter(file);
@@ -91,16 +77,17 @@ public class DriveConect extends IntentService
                 e.printStackTrace();
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-          //  Toast.makeText(this,"print - "+e.getMessage(),Toast.LENGTH_SHORT).show();
+            //  Toast.makeText(this,"print - "+e.getMessage(),Toast.LENGTH_SHORT).show();
         }
-       // Toast.makeText(this,"arquivo baixado com sucesso",Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this,"arquivo baixado com sucesso",Toast.LENGTH_SHORT).show();
 
     }
-    private List<String> downloadRemoteTextFileContent(){
+
+    private List<String> downloadRemoteTextFileContent() {
         URL mUrl = null;
-        HttpURLConnection connection=null;
+        HttpURLConnection connection = null;
         List<String> csvLine = new ArrayList<>();
         TestAdapter mDbHelper = new TestAdapter(this);
         mDbHelper.createDatabase();
@@ -110,42 +97,58 @@ public class DriveConect extends IntentService
             mUrl = new URL(PATH_TO_SERVER);
         } catch (MalformedURLException e) {
             e.printStackTrace();
-          //  Toast.makeText(this,"url - "+e.getMessage(),Toast.LENGTH_SHORT).show();
+            //  Toast.makeText(this,"url - "+e.getMessage(),Toast.LENGTH_SHORT).show();
         }
         try {
             assert mUrl != null;
             connection = (HttpURLConnection) mUrl.openConnection();
             connection.setRequestMethod("GET");
             status = connection.getResponseCode();
-            if ( status == HttpURLConnection.HTTP_OK) {
+            if (status == HttpURLConnection.HTTP_OK) {
                 // method body
                 BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String line = "";
-                boolean test=false;
-                while((line = br.readLine()) != null){
+                boolean test = false;
+                while ((line = br.readLine()) != null) {
                     csvLine.add(line);
-                    String line2 = line.replace(",","','");
-                    if(test){
-                    mDbHelper.insertData("'"+line2+"'");
+                    String line2 = line.replace(",", "','");
+                    if (test) {
+                        mDbHelper.insertData("'" + line2 + "'");
                     }
-                    test=true;
+                    test = true;
                 }
                 br.close();
-            mDbHelper.close();
+                mDbHelper.close();
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
 //            Toast.makeText(this,"conection - "+e.getMessage(),Toast.LENGTH_SHORT).show();
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-  //          Toast.makeText(this,"conection - "+e.getMessage(),Toast.LENGTH_SHORT).show();
-        }finally {
+            //          Toast.makeText(this,"conection - "+e.getMessage(),Toast.LENGTH_SHORT).show();
+        } finally {
             if (connection != null) {
                 connection.disconnect();
             }
         }
         return csvLine;
+    }
+
+    private class DownloadFilesTask extends AsyncTask<URL, Void, List<String>> {
+        protected List<String> doInBackground(URL... urls) {
+            return downloadRemoteTextFileContent();
+        }
+
+        protected void onPostExecute(List<String> result) {
+            if (result != null) {
+                printCVSContent(result);
+                //fileContent.setText("download feito com sucesso");
+                if (status != HttpURLConnection.HTTP_OK) {
+                    //  Toast.makeText(getApplication(),"no connection",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
 
